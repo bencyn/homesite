2ab4dd0f401fdda65fc6d4d589742bdc5d74d9dd
PaulinhoNdegwa <pherndegz@gmail.com>
2018-10-29 12:46:54 +0300

    [Chore #161542231 -  Create project models

---
+++ models.py
@@ -0,0 +1,35 @@
+users = []
+comments = []
+
+
+class User(object):
+    """"Defines user objects and methods"""
+
+    def __init__(self):
+        pass
+
+    def signup(self):
+        pass
+
+    def login(self):
+        pass
+
+    def logout(self):
+        pass
+
+
+class Comment(object):
+    """Defines comment objecta and methods"""
+
+    def __init__(self):
+        pass
+
+    def create_comment(self):
+        pass
+
+    def edit_comment(self):
+        pass
+
+    def delete_comment(self):
+        pass
+
