ea63497bf6d348c501e4720d4a7aef3c67ca35b5
tesh254 <ewachira254@gmail.com>
2018-10-29 15:59:37 +0300

    Final touches

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from datetime import date
+import datetime
 
 users = []
 comments = []
@@ -10,7 +10,7 @@ class User(object):
     def __init__(self):
         self.all_users=users
 
-    def signup(self, username, password, login_time=None,role=None, status=None,):
+    def signup(self, username, password, login_time=None,role=None, status=True):
         
         new_user = {
             "username": username,
@@ -26,7 +26,7 @@ class User(object):
 
 
     def login(self,username,password):
-        current_date = str(date.today())
+        current_date = str(datetime.datetime.utcnow())
         current_user=[user for user in users if user['username']==username]
         if current_user:
             user_password=current_user[0]['password']
@@ -35,8 +35,7 @@ class User(object):
                 current_user[0]['login_time']=current_date
                 user_role=current_user[0]['role']
                 return user_role
-            return None
-	    
+            return None	    
         return None
 
     def logout(self):
@@ -48,37 +47,36 @@ class Comment(object):
     """Defines comment objects and methods"""
 
     def __init__(self):
-        self.comments_list = []
-        self.users = []
+        pass
 
     def create_comment(self, message, username):
         """ add comment """
-        for user in self.users:
+        for user in users:
+            print(users)
             if user['username'] == username:
                 if user['status'] is True: 
-                    incremented_id = len(self.comments_list) + 1
-                    timestamp = datetime.datetime.now()
+                    incremented_id = len(comments) + 1
+                    timestamp = datetime.datetime.utcnow()
                     comment = {
                     "id": incremented_id,
                     "message": message, 
                     "timestamp":timestamp,
                     "author":username
                     }
-                    self.comments_list.append(comment)
-            return " you must be logged in to make comment"
-        return "user does not exist"
-
-    def create_comment(self):
-        pass
+                    comments.append(comment)
+                    return comment
+                return None
+            return None
+        return None
 
     def edit_comment(self, id):
         new = input('edit the message')
-        for comment in self.comments:
+        for comment in comments:
             if comment['id'] == id:
                 comment.update(new)
 
 
     def delete_comment(self, id):
-        for i, comment in enumerate(self.comments):
-            if orcomment['id'] == id:
-                self.comments.pop(i)
+        for i, comment in enumerate(comments):
+            if comment['id'] == id:
+                comments.pop(i)
--- run.py
+++ run.py
@@ -8,7 +8,7 @@ comment = Comment()
 def main():
     role = ''
     status = False
-    username = null
+    username = None
     while True:
         print("""
         ----------------------------------------------------
@@ -25,42 +25,42 @@ def main():
         | 'edit-own': To edit own                           |
        -----------------------------------------------------
         """)
-        command = str(input("Enter a command? "))
+        command = str(input("Enter a option? "))
         if command == "login":
-            username = str(input("Enter your email: "))
+            username_ = str(input("Enter your username: "))
             password = str(input("Enter your password"))
             if user.login(username, password):
                 print("Logged in successfully")
                 status = True
-                command = str(input("Enter a command? "))
-                role = user.login(username, password)
+                command = str(input("Enter a option? "))
+                role = user.login(username_, password)
                 if role == "admin":
                     print("Admin welcome")
                     username = username_
-                    command = str(input("Enter a command? "))
+                    command = str(input("Enter a option? "))
                     if command == "delete":
                         comment_id = int(input("Enter the id of the comment you want to delete: "))
                         if comment.delete_comment(comment_id):
                             print("Comment deleted successfully")
-                            command = str(input("Enter a command? "))
+                            command = str(input("Enter a option? "))
                         else: 
                             print("Comment of that id was not found, try another command to try again")
-                            command = str(input("Enter command? "))
+                            command = str(input("Enter option? "))
                     elif command == "edit":
                         comment_id = int(input("Enter the id of the comment you want to delete: "))
                         if comment.edit_comment(comment_id):
                             print("Comment updated successfully")
-                            command = str(input("Enter a command? "))
+                            command = str(input("Enter a option? "))
                         else: 
                             print("Comment of that id was not found, try another command to try again")
-                            command = str(input("Enter command? "))
+                            command = str(input("Enter option? "))
                     elif command == "comment":
                         comment_msg = str(input("Enter the message you would like to comment: "))
                         comment__ = comment.create_comment(comment_msg, username)
                         print("""comment created successfully
                                     {}
                         """.format(comment__))
-                        command = str(input("Enter a command? "))
+                        command = str(input("Enter a option? "))
                     else:
                         print("""
                         ------------Unfortunately we do not get your command--------------
@@ -81,139 +81,149 @@ def main():
                         command = str(input("Try again by entering a command: "))
                 elif role== "User":
                     print("User Welcome")
-                    command = str(input("Enter a command? "))
+                    command = str(input("Enter a option? "))
                     if command == "comment":
                         comment_msg = str(input("Enter the message you would like to comment: "))
                         comment__ = comment.create_comment(comment_msg, username)
                         print("""comment created successfully
                                     {}
                         """.format(comment__))
-                        command = str(input("Enter a command? "))
+                        command = str(input("Enter a option? "))
                     elif command =="edit-own":
                         print("Not yet handled")
                 elif role== "moderator":
                     print("Moderator Welcome")
-                    command = str(input("Enter a command? "))
+                    command = str(input("Enter a option? "))
                     if command == "delete":
                         comment_id = int(input("Enter the id of the comment you want to delete: "))
                         if comment.delete_comment(comment_id):
                             print("Comment deleted successfully")
-                            command = str(input("Enter a command? "))
+                            command = str(input("Enter a option? "))
                         else: 
                             print("Comment of that id was not found, try another command to try again")
-                            command = str(input("Enter command? "))   
+                            command = str(input("Enter option? "))   
                     else:
                         print("I am not getting you right now")
             else:
-                print("Your role is not identified by the system try another command")
-                command = str(input("Enter a command? "))
+                print("username was not found")
+                command = str(input("Enter a option? "))
         elif command == "signup":
             username_ = str(input("Enter your new username: "))
-            password = str(input("Enter your username: "))
+            password = str(input("Enter your password: "))
             role_ = str(input("Enter your user role: ")).lower()
             status = True
             if user.signup(username_, password, role, status):
-                print("Account succefully created")
-                role = role_
-                if role == "admin":
-                    print("Admin welcome")
-                    username = username_
-                    command = str(input("Enter a command? "))
-                    if command == "delete":
-                        comment_id = int(input("Enter the id of the comment you want to delete: "))
-                        if comment.delete_comment(comment_id):
-                            print("Comment deleted successfully")
-                            command = str(input("Enter a command? "))
-                        else: 
-                            print("Comment of that id was not found, try another command to try again")
-                            command = str(input("Enter command? "))
-                    elif command == "edit":
-                        comment_id = int(input("Enter the id of the comment you want to delete: "))
-                        if comment.edit_comment(comment_id):
-                            print("Comment updated successfully")
-                            command = str(input("Enter a command? "))
-                        else: 
-                            print("Comment of that id was not found, try another command to try again")
-                            command = str(input("Enter command? "))
-                    elif command == "comment":
-                        comment_msg = str(input("Enter the message you would like to comment: "))
-                        comment__ = comment.create_comment(comment_msg, username)
-                        print("""comment created successfully
-                                    {}
-                        """.format(comment__))
-                        command = str(input("Enter a command? "))
-                    else:
-                        print("""
-                        ------------Unfortunately we do not get your command--------------
-                        ----------------------------------------------------
-                        |                This are the commands accepted     |
-                        | Help: List of commands for a better experience at |
-                        | Homesite 'We serve better than the competition    |
-                        | 'login' : Type this to login                      |
-                        | 'signup' : Type this to signup                    |
-                        | 'comment' :Type this to comment                   |
-                        | 'edit' : Type this to edit a comment              |
-                        | 'delete' : Type this to delete a comment,         |
-                        | 'quit' : To leave the program                     |
-                        | 'logout' : To logout of the site                  |
-                        | 'edit-own': To edit own                           |
-                        -----------------------------------------------------
-                        """)
-                        command = str(input("Try again by entering a command: "))
-                elif role== "User":
-                    print("User Welcome")
-                    command = str(input("Enter a command? "))
-                    if command == "comment":
-                        comment_msg = str(input("Enter the message you would like to comment: "))
-                        comment__ = comment.create_comment(comment_msg, username)
-                        print("""comment created successfully
-                                    {}
-                        """.format(comment__))
-                        command = str(input("Enter a command? "))
-                    elif command =="edit-own":
-                        print("Not yet handled")
-                elif role== "moderator":
-                    print("Moderator Welcome")
-                    command = str(input("Enter a command? "))
-                    if command == "delete":
-                        comment_id = int(input("Enter the id of the comment you want to delete: "))
-                        if comment.delete_comment(comment_id):
-                            print("Comment deleted successfully")
-                            command = str(input("Enter a command? "))
-                        else: 
-                            print("Comment of that id was not found, try another command to try again")
-                            command = str(input("Enter command? "))       
-                    elif command== "comment":
-                        comment_msg = str(input("Enter the message you would like to comment: "))
-                        comment__ = comment.create_comment(comment_msg, username)
-                        print("""comment created successfully
-                                    {}
-                        """.format(comment__))
-                        command = str(input("Enter a command? "))
-                    else:
-                        print("""
-                        ------------Unfortunately we do not get your command--------------
-                        ----------------------------------------------------
-                        |                This are the commands accepted     |
-                        | Help: List of commands for a better experience at |
-                        | Homesite 'We serve better than the competition    |
-                        | 'login' : Type this to login                      |
-                        | 'signup' : Type this to signup                    |
-                        | 'comment' :Type this to comment                   |
-                        | 'edit' : Type this to edit a comment              |
-                        | 'delete' : Type this to delete a comment,         |
-                        | 'quit' : To leave the program                     |
-                        | 'logout' : To logout of the site                  |
-                        | 'edit-own': To edit own                           |
-                        -----------------------------------------------------
-                        """)
-                        command = str(input("Try again by entering a command: "))         
+                if role_ not in ["admin", "moderator", "user"]:
+                    print("Role unidentified")
+                    command = str(input("Enter an option? "))
+                else:                    
+                    print("Account succefully created")
+                    role = role_
+                    if role == "admin":
+                        print("Admin welcome")
+                        username = username_
+                        command = str(input("Enter a option? "))
+                        if command == "delete":
+                            comment_id = int(input("Enter the id of the comment you want to delete: "))
+                            if comment.delete_comment(comment_id):
+                                print("Comment deleted successfully")
+                                command = str(input("Enter a option? "))
+                            else: 
+                                print("Comment of that id was not found, try another command to try again")
+                                command = str(input("Enter option? "))
+                        elif command == "edit":
+                            comment_id = int(input("Enter the id of the comment you want to delete: "))
+                            if comment.edit_comment(comment_id):
+                                print("Comment updated successfully")
+                                command = str(input("Enter a option? "))
+                            else: 
+                                print("Comment of that id was not found, try another command to try again")
+                                command = str(input("Enter option? "))
+                        elif command == "comment":
+                            comment_msg = str(input("Enter the message you would like to comment: "))
+                            comment__ = comment.create_comment(comment_msg, username)
+                            print("""comment created successfully
+                                        {}
+                            """.format(comment__))
+                            command = str(input("Enter a option? "))
+                        else:
+                            print("""
+                            ------------Unfortunately we do not get your command--------------
+                            ----------------------------------------------------
+                            |                This are the commands accepted     |
+                            | Help: List of commands for a better experience at |
+                            | Homesite 'We serve better than the competition    |
+                            | 'login' : Type this to login                      |
+                            | 'signup' : Type this to signup                    |
+                            | 'comment' :Type this to comment                   |
+                            | 'edit' : Type this to edit a comment              |
+                            | 'delete' : Type this to delete a comment,         |
+                            | 'quit' : To leave the program                     |
+                            | 'logout' : To logout of the site                  |
+                            | 'edit-own': To edit own                           |
+                            -----------------------------------------------------
+                            """)
+                            command = str(input("Try again by entering a command: "))
+                    elif role== "User":
+                        print("User Welcome")
+                        command = str(input("Enter a option? "))
+                        if command == "comment":
+                            comment_msg = str(input("Enter the message you would like to comment: "))
+                            comment__ = comment.create_comment(comment_msg, username)
+                            print("""comment created successfully
+                                        {}
+                            """.format(comment__))
+                            command = str(input("Enter a option? "))
+                        elif command =="edit-own":
+                            print("Not yet handled")
+                    elif role== "moderator":
+                        print("Moderator Welcome")
+                        command = str(input("Enter a option? "))
+                        if command == "delete":
+                            comment_id = int(input("Enter the id of the comment you want to delete: "))
+                            if comment.delete_comment(comment_id):
+                                print("Comment deleted successfully")
+                                command = str(input("Enter a option? "))
+                            else: 
+                                print("Comment of that id was not found, try another command to try again")
+                                command = str(input("Enter option? "))       
+                        elif command== "comment":
+                            comment_msg = str(input("Enter the message you would like to comment: "))
+                            comment__ = comment.create_comment(comment_msg, username)
+                            print("""comment created successfully
+                                        {}
+                            """.format(comment__))
+                            command = str(input("Enter a option? "))
+                        else:
+                            print("""
+                            ------------Unfortunately we do not get your command--------------
+                            ----------------------------------------------------
+                            |                This are the commands accepted     |
+                            | Help: List of commands for a better experience at |
+                            | Homesite 'We serve better than the competition    |
+                            | 'login' : Type this to login                      |
+                            | 'signup' : Type this to signup                    |
+                            | 'comment' :Type this to comment                   |
+                            | 'edit' : Type this to edit a comment              |
+                            | 'delete' : Type this to delete a comment,         |
+                            | 'quit' : To leave the program                     |
+                            | 'logout' : To logout of the site                  |
+                            | 'edit-own': To edit own                           |
+                            -----------------------------------------------------
+                            """)
+                            command = str(input("Try again by entering a command: "))         
             else:
                 print("""
                 ---------------------------------
                 Password: at least four characters
                 """)
-                command = str(input("Enter a command? "))
+                command = str(input("Enter a option? "))
+        elif command =="quit":
+            print("Bye, thanks for visiting")
+            break
+        elif command =="logout":
+            print("Bye, thanks for visiting, log out successfull")
+            break
         else:
             print("""
             ------------Unfortunately we do not get your command--------------
@@ -231,3 +241,6 @@ def main():
             -----------------------------------------------------
             """)
             command = str(input("Try again by entering a command: "))
+
+if  __name__ == "__main__":
+    main()
